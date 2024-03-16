6da7c506fedd9a128835fbd85ead312eb01a3a7a
chalanasangeeth36 <chalanasthisera@gmail.com>
2024-03-16 09:27:49 +0530

    about page updated

--- src/app/(pages)/_page-components/about/AboutSection2.tsx
+++ src/app/(pages)/_page-components/about/AboutSection2.tsx
@@ -5,38 +5,154 @@ import Title from "@/components/common/texts/Title";
 import Image from "next/image";
 
 export default function AboutSection2() {
-  const Content = `
-    We envision a world where every person can find their soulmate, build a strong foundation, and embark on a lifelong journey of love and companionship. We are dedicated to making this vision a reality by providing a safe, inclusive, and supportive platform for individuals seeking marriage.`;
+  // our vision
+  const OurVision = `We envision a world where every person can find their soulmate, build a strong foundation, and embark on a lifelong journey of love and companionship. We are dedicated to making this vision a reality by providing a safe, inclusive, and supportive platform for individuals seeking marriage.`;
+  // our mission
+  const OurMission = `At Cupidco, we're on a mission to help you find true love and create lifelong partnerships. Our platform is dedicated to guiding you on the path to a happy and fulfilling marriage`;
+
+  // our Story description 01
+  const OurStoryDescription1 = `[Your Founder's Name], the founder of [Your Platform Name], started this platform with a deep passion for helping people find their perfect life partners. Their personal experiences and a desire to make a positive impact on the world led to the creation of Cupidco`;
+  // our Story description 02
+  const OurStoryDescription2 = `[Your Founder's Name], the founder of [Your Platform Name], started this platform with a deep passion for helping people find their perfect life partners. Their personal experiences and a desire to make a positive impact on the world led to the creation of Cupidco`;
 
   return (
-    <div className="bg-[#FFF4F1] min-h-auto p-20 max-md:p-10">
-      <div className="grid md:grid-cols-2 text-justify items-center gap-5">
-        <div className="flex justify-center">
-          <Image
-            src="/images/SampleRectangle.png"
-            alt="Double Images"
-            width={300}
-            height={50}
-            className="w-[80%]"
-          />
-        </div>
-        <div className="flex flex-col max-sm:gap-5 md:gap-3 lg:gap-3 max-xl:gap-3">
-          <div>
+    <div className="bg-[#FFF4F1] text-justify px-8 md:px-16 py-10 md:py-20 ">
+      {/* our vision title, description and image */}
+      <div className="grid md:grid-cols-2  ">
+        <div className="  md:order-last">
+          {/* background image  */}
+          <div className="absolute mt-10 max-lg:hidden lg:ml-10">
+            <Image
+              src={"/images/pngwing.png"}
+              alt={" text background image"}
+              width={500}
+              height={50}
+            />
+          </div>
+
+          {/* title  */}
+          <div className="mb-3 md:flex ">
             <Title
               text="Our Vision"
               center={true}
               fontSize="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl"
             />
           </div>
-          <Description text={Content} />
+          {/* description  */}
+          <div className="mb-5">
+            <Description text={OurVision} />
+          </div>
+
+          {/* our mission in our vision section  */}
+          <div className="max-lg:hidden">
+            <div>
+              {/* title  */}
+              <div className="mt-16 mb-4 lg:flex lg:justify-end">
+                <Title
+                  text="Our Mission"
+                  center={true}
+                  fontSize="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl"
+                />
+              </div>
+            </div>
+            {/* description  */}
+            <div className="mb-5">
+              <Description text={OurMission} />
+            </div>
+          </div>
+        </div>
+        {/* image  */}
+        <div className="flex justify-center">
+          <Image
+            src="/images/AboutSectionImage.png"
+            alt="Double Images"
+            width={300}
+            height={50}
+            className="shadow-2xl max-lg:w-48"
+          />
+        </div>
+      </div>
+      {/* our Mission title, description and image */}
+      <div className="md:grid md:grid-cols-2 lg:hidden">
+        <div>
           <div>
+            {/* title  */}
+            <div className="mt-16 mb-4 md:flex ">
+              <Title
+                text="Our Mission"
+                center={true}
+                fontSize="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl"
+              />
+            </div>
+          </div>
+          {/* description  */}
+          <div className="mb-5">
+            <Description text={OurMission} />
+          </div>
+        </div>
+        {/* image  */}
+        <div className="max-md:hidden flex justify-center lg:hidden">
+          <Image
+            src="/images/AboutSectionImage.png"
+            alt="Double Images"
+            width={200}
+            height={50}
+            className="shadow-2xl"
+          />
+        </div>
+      </div>
+
+      {/* Our Story title, dedescription and image  */}
+      <div className="lg:grid lg:grid-cols-2 lg:mt-10">
+        <div>
+          {/* background image  */}
+          <div className="absolute mt-20 max-lg:hidden ml-5 ">
+            <Image
+              src={"/images/pngwing.png"}
+              alt={" text background image"}
+              width={500}
+              height={50}
+            />
+          </div>
+          {/* title  */}
+          <div className="mt-16 mb-4">
             <Title
-              text="Our Mission"
+              text="Our Story"
               center={true}
               fontSize="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl"
             />
           </div>
-          <Description text={Content} />
+          {/* description 1 */}
+          <div className="mb-5">
+            <Description text={OurStoryDescription1} />
+          </div>
+
+          {/* description 1 */}
+          <div className="mb-2">
+            <Description text={OurStoryDescription2} />
+          </div>
+        </div>
+
+        {/* image  */}
+        <div className="md:flex md:gap-5 lg:gap-0 md:justify-center md:mt-5 ">
+          <div className="max-md:mr-28 max-md:mt-5 flex max-md:justify-center  lg:mr-[-20px]">
+            <Image
+              src="/images/AboutSectionImage.png"
+              alt="Double Images"
+              width={150}
+              height={50}
+              className="shadow-2xl md:w-60 lg:w-52 lg:h-72"
+            />
+          </div>
+          <div className="max-md:ml-28 max-md:mt-[-90px] flex max-md:justify-center lg:ml-[-20px]">
+            <Image
+              src="/images/AboutSectionImage.png"
+              alt="Double Images"
+              width={150}
+              height={50}
+              className="shadow-2xl md:w-60 lg:w-52 lg:h-72 lg:flex lg:mt-40 "
+            />
+          </div>
         </div>
       </div>
     </div>
